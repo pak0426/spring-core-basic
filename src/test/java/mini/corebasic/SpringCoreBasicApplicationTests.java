0683735ccae33183dc119c12636af9ee6f89a863
hmmini <pak4184@naver.com>
2024-03-11 01:45:02 +0900

    first commit

---
+++ src/test/java/mini/corebasic/SpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package mini.corebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
